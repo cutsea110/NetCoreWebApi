92e8996b30424bbfba07ca08e03957f7295b3240
cutsea110 <cutsea110@gmail.com>
2019-08-02 10:06:28 +0900

    [Refactor]

--- ValuesController.cs
+++ ValuesController.cs
@@ -5,7 +5,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
